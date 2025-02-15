313a7973e690968e8ccad694cc8f2b4dca999eb8
SoumenCodes <soumen19j@gmail.com>
2025-02-15 13:49:17 +0530

    login screen

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,61 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import images from "@/constants/images";
+import icons from "@/constants/icons";
 
 const SignIn = () => {
+  const handleLogin = () => {};
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+        <View className="px-10">
+          <Text className="text-base text-center uppercase font-rubik text-black">
+            Welcome to Real State
+          </Text>
+          <Text className="text-3xl font-rubik-bold text-center text-black mt-2">
+            Let's Get Closer to {"\n"}
+          </Text>
+          <Text className="text-primary-300 text-center text-2xl font-rubik-bold ">
+            Your Ideal Home
+          </Text>
+          <Text className="text-lg font-rubik text-black text-center mt-12">
+            Login To Real Estate with Google
+          </Text>
+          <TouchableOpacity
+            className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5"
+            onPress={handleLogin}
+          >
+            <View className="flex flex-row items-center justify-center">
+              <Image
+                source={icons.google}
+                className="h-5 w-5 "
+                resizeMode="contain"
+              />
+              <Text className="text-lg font-rubik text-black ml-2">
+                Continue With Google
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
